ddc7823e09edeeb864505c139f8892327cc5b5fa
BarbaraBlue <barbaravaldesnunez@gmail.com>
2019-01-15 14:54:11 -0300

    Extracción de títulos, duración de videos, y prueba (con errores) para extraer las views.

--- Extrayendo_titulos.R
+++ Extrayendo_titulos.R
@@ -1,6 +1,17 @@
+library("rvest")
+
+###Instalando mil paquetes para ver si sirve el Xpath####
+install.packages("selectr")
+install.packages("RCulr")
+install.packages("XML")
+
+library("RCulr")
+library("selectr")
+library("XML")
+
 #==================== usando xvideos ====================#
 
-paginaXVideos <- 'https://www.xvideos.com/new/2'
+paginaXVideos <- 'https://www.xvideos.com/new/1/'
 
 webpageXVideos <- read_html(paginaXVideos)
 
@@ -18,6 +29,12 @@ print(textoXVideos)
 
 # Viendo que tiene la posición 1 la variable textoXVideos
 print(textoXVideos[1])
+
+# Tabla de los titulos de la pág 1 de new 
+tabla_titulos <- table(textoXVideos)
+
+# Transformando a data framtabla
+tituloXVideos <- as.data.frame(tabla_titulos)
 
 # Unificando los títulos
 todosLosTitulosXVideo <- ""
@@ -37,3 +54,29 @@ tablaXVideos <- table(unlistTitulosXVideos)
 
 # Transformando a data framtabla
 tituloXVideos <- as.data.frame(tablaXVideos)
+
+
+#Prueba extraccion de duracion de cada video
+DurationXVideos <- html_nodes(webpageXVideos,'.duration')
+DuracionXVideos <- html_text(DurationXVideos)
+
+#Duracion de videos en tabla
+tabla_duracion <- table(DuracionXVideos)
+Duracion_tabla <- as.data.frame(tabla_duracion)
+
+####INTENTANDO EXTRAER VIEWS#####NADA SIRVE### AYUDA####
+
+views <- getNodeSet(webpageXVideos,"//*[@id="video_43754597"]/div[2]/p[2]/span/span[2]","x")
+
+views <- webpageXVideos(Xpath="//*[@id="video_43754597"]/div[2]/p[2]/span/span[2]")
+
+vistaql <- webpageXVideos %>% html_nodes(xpath = ‘//*[@id="video_43754597"]/div[2]/p[2]/span/span[2]’) %>% html_text()
+ 
+getNodeSet(webpageXVideos, "//*[@id="video_43754597"]/div[2]/p[2]/span/span[2]")
+
+viewssss <- html_nodes(webpageXVideos, xpath="//*[@id="video_43754597"]/div[2]/p[2]/span/span[2]")
+
+getNodeSet(//*[@id="video_43721271"]/div[2]/p[2]/span/span[2]
+
+webpageXVideos
+tratamientos <- html.definicion.temp %>% html_nodes(xpath = ‘//div[contains(@id,”main”)]’) %>% html_text()
